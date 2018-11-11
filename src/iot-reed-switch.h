d32269ee94377a117f850b0b2807ae2cb5024fb9
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2018-11-11 20:20:42 +0100

    added lib

--- src/iot-reed-switch.h
+++
@@ -1,9 +0,0 @@
-#ifndef iotReedSwitch_h
-#define iotReedSwitch_h
-
-namespace bmw12
-{
-    bool reedSwitchGet(const int pin, const int tryCount = 3, const int waitMilis = 10);
-} // namespace bmw12
-
-#endif
